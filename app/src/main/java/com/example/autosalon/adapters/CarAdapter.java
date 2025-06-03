e787b29ee0ed5d1b4dcd59b6a8acefe0a35e07d3
PzKpfw-ausf-H <nikitanovozhilovvv@gmail.com>
2025-06-03 16:24:56 +0300

    Added pictures to the car cards

--- app/src/main/java/com/example/autosalon/adapters/CarAdapter.java
+++ app/src/main/java/com/example/autosalon/adapters/CarAdapter.java
@@ -57,7 +57,7 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
         holder.priceText.setText(car.getPrice());
 
         // Подгружаем изображение
-        Glide.with(context).load(car.getImageUrl()).into(holder.imageView);
+        holder.imageView.setImageResource(getLocalImageResource(car.getModelName()));
 
         // Слушатель нажатия на карточку
         holder.itemView.setOnClickListener(v -> listener.onItemClick(car));
@@ -75,6 +75,31 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
                     newState ? "Добавлено в избранное" : "Удалено из избранного",
                     Toast.LENGTH_SHORT).show();
         });
+    }
+
+    private int getLocalImageResource(String modelName) {
+        switch (modelName.toLowerCase()) {
+            case "camry":
+                return R.drawable.camry;
+            case "bmw 3 series":
+                return R.drawable.bmw3;
+            case "kia rio":
+                return R.drawable.rio;
+            case "hyundai solaris":
+                return R.drawable.solaris;
+            case "audi a4":
+                return R.drawable.audi_a4;
+            case "tesla model 3":
+                return R.drawable.tesla3;
+            case "lada vesta":
+                return R.drawable.lada;
+            case "mustang gt":
+                return R.drawable.mustang;
+            case "model s":
+                return R.drawable.teslas;
+            default:
+                return R.drawable.placeholder;
+        }
     }
 
     private void updateFavoriteIcon(ImageView icon, boolean isFavorite) {
